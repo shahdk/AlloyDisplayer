2eba94cf85f26e2ec1d189451806529961991bc7
shahdk <shahdk@rose-hulman.edu>
2014-09-05 13:35:20 -0400

    Added new files

---
+++ AlloyDisplayer/src/TestClass.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TestClass {
+
+	@Test
+	public void test() {
+		Assert.assertEquals(0, 0);
+	}
+
+}
